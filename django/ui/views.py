bf335f9a8ec65b988f3edeb1bb0e2eca4ecbeb1a
Daniel Wiratman <74503671+danielwiratman@users.noreply.github.com>
2023-05-15 22:42:51 +0700

    working dashboard

--- django/ui/views.py
+++ django/ui/views.py
@@ -47,105 +47,105 @@ def api(request):
             "subsistem1": {
                 "kadarproteinsusu": Sensor.objects.filter(
                     pabrik=1, subsistem=1, name="kadarproteinsusu"
-                ).reverse()[0].value,
+                ).last().value,
                 "kekentalansusu": Sensor.objects.filter(
                     pabrik=1, subsistem=1, name="kekentalansusu"
-                ).reverse()[0].value,
+                ).last().value,
                 "jumlahkuningtelur": Sensor.objects.filter(
                     pabrik=1, subsistem=1, name="jumlahkuningtelur"
-                ).reverse()[0].value,
+                ).last().value,
             },
             "subsistem2": {
                 "kemerahandaging": Sensor.objects.filter(
                     pabrik=1, subsistem=2, name="kemerahandaging"
-                ).reverse()[0].value,
+                ).last().value,
                 "kadarlemak": Sensor.objects.filter(
                     pabrik=1, subsistem=2, name="kadarlemak"
-                ).reverse()[0].value,
+                ).last().value,
                 "kadarair": Sensor.objects.filter(
                     pabrik=1, subsistem=2, name="kadarair"
-                ).reverse()[0].value,
+                ).last().value,
             },
             "subsistem3": {
                 "microbacteria": Sensor.objects.filter(
                     pabrik=1, subsistem=3, name="microbacteria"
-                ).reverse()[0].value,
+                ).last().value,
                 "kadarprotein": Sensor.objects.filter(
                     pabrik=1, subsistem=3, name="kadarprotein"
-                ).reverse()[0].value,
+                ).last().value,
                 "keasamandaging": Sensor.objects.filter(
                     pabrik=1, subsistem=3, name="keasamandaging"
-                ).reverse()[0].value,
+                ).last().value,
             },
         },
         "pabrik2": {
             "subsistem1": {
                 "kekuninganpadi": Sensor.objects.filter(
                     pabrik=2, subsistem=1, name="kekuninganpadi"
-                ).reverse()[0].value,
+                ).last().value,
                 "kebulatanpadi": Sensor.objects.filter(
                     pabrik=2, subsistem=1, name="kebulatanpadi"
-                ).reverse()[0].value,
+                ).last().value,
                 "kadarkarbohidrat": Sensor.objects.filter(
                     pabrik=2, subsistem=1, name="kadarkarbohidrat"
-                ).reverse()[0].value,
+                ).last().value,
             },
             "subsistem2": {
                 "kehijauan": Sensor.objects.filter(
                     pabrik=2, subsistem=2, name="kehijauan"
-                ).reverse()[0].value,
+                ).last().value,
                 "kadarklorofil": Sensor.objects.filter(
                     pabrik=2, subsistem=2, name="kadarklorofil"
-                ).reverse()[0].value,
+                ).last().value,
                 "amonia": Sensor.objects.filter(
                     pabrik=2, subsistem=2, name="amonia"
-                ).reverse()[0].value,
+                ).last().value,
             },
             "subsistem3": {
                 "etilen": Sensor.objects.filter(
                     pabrik=2, subsistem=3, name="etilen"
-                ).reverse()[0].value,
+                ).last().value,
                 "kadargula": Sensor.objects.filter(
                     pabrik=2, subsistem=3, name="kadargula"
-                ).reverse()[0].value,
+                ).last().value,
                 "kepadatan": Sensor.objects.filter(
                     pabrik=2, subsistem=3, name="kepadatan"
-                ).reverse()[0].value,
+                ).last().value,
             },
         },
         "pabrik3": {
             "subsistem1": {
                 "suhu": Sensor.objects.filter(
                     pabrik=3, subsistem=1, name="suhu"
-                ).reverse()[0].value,
+                ).last().value,
                 "arahangin": Sensor.objects.filter(
                     pabrik=3, subsistem=1, name="arahangin"
-                ).reverse()[0].value,
+                ).last().value,
                 "curahhujan": Sensor.objects.filter(
                     pabrik=3, subsistem=1, name="curahhujan"
-                ).reverse()[0].value,
+                ).last().value,
             },
             "subsistem2": {
                 "kasiridle": Sensor.objects.filter(
                     pabrik=3, subsistem=2, name="kasiridle"
-                ).reverse()[0].value,
+                ).last().value,
                 "ratepenjualan": Sensor.objects.filter(
                     pabrik=3, subsistem=2, name="ratepenjualan"
-                ).reverse()[0].value,
+                ).last().value,
                 "jumlahpembayaran": Sensor.objects.filter(
                     pabrik=3, subsistem=2, name="jumlahpembayaran"
-                ).reverse()[0].value,
+                ).last().value,
             },
             "subsistem3": {
                 "waiteridle": Sensor.objects.filter(
                     pabrik=3, subsistem=3, name="waiteridle"
-                ).reverse()[0].value,
+                ).last().value,
                 "okupansirestoran": Sensor.objects.filter(
                     pabrik=3, subsistem=3, name="okupansirestoran"
-                ).reverse()[0].value,
+                ).last().value,
                 "stokpiring": Sensor.objects.filter(
                     pabrik=3, subsistem=3, name="stokpiring"
-                ).reverse()[0].value,
+                ).last().value,
             }
         },
     }
@@ -158,37 +158,37 @@ client.message_callback_add("prauas/pabrik1/subsistem1/kadarproteinsusu", on_mes
 client.message_callback_add("prauas/pabrik1/subsistem1/kekentalansusu", on_message_112)
 client.message_callback_add("prauas/pabrik1/subsistem1/jumlahkuningtelur", on_message_113)
 
-client.message_callback_add("prauas/pabrik1/subsistem2/kadarproteinsusu", on_message_121)
-client.message_callback_add("prauas/pabrik1/subsistem2/kekentalansusu", on_message_122)
-client.message_callback_add("prauas/pabrik1/subsistem2/jumlahkuningtelur", on_message_123)
+client.message_callback_add("prauas/pabrik1/subsistem2/kemerahandaging", on_message_121)
+client.message_callback_add("prauas/pabrik1/subsistem2/kadarlemak", on_message_122)
+client.message_callback_add("prauas/pabrik1/subsistem2/kadarair", on_message_123)
 
-client.message_callback_add("prauas/pabrik1/subsistem3/kadarproteinsusu", on_message_131)
-client.message_callback_add("prauas/pabrik1/subsistem3/kekentalansusu", on_message_132)
-client.message_callback_add("prauas/pabrik1/subsistem3/jumlahkuningtelur", on_message_133)
+client.message_callback_add("prauas/pabrik1/subsistem3/microbacteria", on_message_131)
+client.message_callback_add("prauas/pabrik1/subsistem3/kadarprotein", on_message_132)
+client.message_callback_add("prauas/pabrik1/subsistem3/keasamandaging", on_message_133)
 
-client.message_callback_add("prauas/pabrik2/subsistem1/kadarproteinsusu", on_message_211)
-client.message_callback_add("prauas/pabrik2/subsistem1/kekentalansusu", on_message_212)
-client.message_callback_add("prauas/pabrik2/subsistem1/jumlahkuningtelur", on_message_213)
+client.message_callback_add("prauas/pabrik2/subsistem1/kekuninganpadi", on_message_211)
+client.message_callback_add("prauas/pabrik2/subsistem1/kebulatanpadi", on_message_212)
+client.message_callback_add("prauas/pabrik2/subsistem1/kadarkarbohidrat", on_message_213)
 
-client.message_callback_add("prauas/pabrik2/subsistem2/kadarproteinsusu", on_message_221)
-client.message_callback_add("prauas/pabrik2/subsistem2/kekentalansusu", on_message_222)
-client.message_callback_add("prauas/pabrik2/subsistem2/jumlahkuningtelur", on_message_223)
+client.message_callback_add("prauas/pabrik2/subsistem2/kehijauan", on_message_221)
+client.message_callback_add("prauas/pabrik2/subsistem2/kadarklorofil", on_message_222)
+client.message_callback_add("prauas/pabrik2/subsistem2/amonia", on_message_223)
 
-client.message_callback_add("prauas/pabrik3/subsistem3/kadarproteinsusu", on_message_231)
-client.message_callback_add("prauas/pabrik3/subsistem3/kekentalansusu", on_message_232)
-client.message_callback_add("prauas/pabrik3/subsistem3/jumlahkuningtelur", on_message_233)
+client.message_callback_add("prauas/pabrik2/subsistem3/etilen", on_message_231)
+client.message_callback_add("prauas/pabrik2/subsistem3/kadargula", on_message_232)
+client.message_callback_add("prauas/pabrik2/subsistem3/kepadatan", on_message_233)
 
-client.message_callback_add("prauas/pabrik3/subsistem1/kadarproteinsusu", on_message_311)
-client.message_callback_add("prauas/pabrik3/subsistem1/kekentalansusu", on_message_312)
-client.message_callback_add("prauas/pabrik3/subsistem1/jumlahkuningtelur", on_message_313)
+client.message_callback_add("prauas/pabrik3/subsistem1/suhu", on_message_311)
+client.message_callback_add("prauas/pabrik3/subsistem1/arahangin", on_message_312)
+client.message_callback_add("prauas/pabrik3/subsistem1/curahhujan", on_message_313)
 
-client.message_callback_add("prauas/pabrik3/subsistem2/kadarproteinsusu", on_message_321)
-client.message_callback_add("prauas/pabrik3/subsistem2/kekentalansusu", on_message_322)
-client.message_callback_add("prauas/pabrik3/subsistem2/jumlahkuningtelur", on_message_323)
+client.message_callback_add("prauas/pabrik3/subsistem2/kasiridle", on_message_321)
+client.message_callback_add("prauas/pabrik3/subsistem2/ratepenjualan", on_message_322)
+client.message_callback_add("prauas/pabrik3/subsistem2/jumlahpembayaran", on_message_323)
 
-client.message_callback_add("prauas/pabrik3/subsistem3/kadarproteinsusu", on_message_331)
-client.message_callback_add("prauas/pabrik3/subsistem3/kekentalansusu", on_message_332)
-client.message_callback_add("prauas/pabrik3/subsistem3/jumlahkuningtelur", on_message_333)
+client.message_callback_add("prauas/pabrik3/subsistem3/waiteridle", on_message_331)
+client.message_callback_add("prauas/pabrik3/subsistem3/okupansirestoran", on_message_332)
+client.message_callback_add("prauas/pabrik3/subsistem3/stokpiring", on_message_333)
 
 client.connect("localhost", 1883)
 client.subscribe("prauas/#")
